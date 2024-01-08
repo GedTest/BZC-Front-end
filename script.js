1febc0c3a666c80ac258d4b5b365b58034839b9e
GedTest <viktorzwinger@seznam.cz>
2024-01-08 19:27:00 +0100

    prettier calendar, text aligned to center center, cells are square with ratio 1:1, added name of month and buttons

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {    
     const calendar = document.querySelector('#calendar');
+    const daysContainer = document.querySelector('.container');
     let currentDate = new Date();
     
-    let currentMonth = 2;
-    const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth()+(currentMonth+1), 0);
-    const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth()+currentMonth, 1);
+    let currentMonth = 11;
 
-    const weekday = ["NE","PO","ÚT","ST","ČT","PÁ","SO"];
 
+    let lastDayOfMonth = 1;
+    let firstDayOfMonth = 30;
+
+
+    function showCalendar() {
+        lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth()+(currentMonth+1), 0);
+        firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth()+currentMonth, 1);
+
+
+        const months = [
+            "Leden","Únor","Březen",
+            "Duben","Květen","Červen",
+            "Červenec","Srpen","Září",
+            "Říjen","Listopad","Prosinec"
+        ];
+        const month = document.querySelector('#monthYear')
+        month.innerText = `${months[firstDayOfMonth.getMonth()]} ${firstDayOfMonth.getFullYear()}`;
+
+        createCalendarGrid();
+    }
     function createCalendarGrid() {
-        const daysContainer = document.createElement('div');
-        daysContainer.classList.add('days');
+        daysContainer.innerHTML = '';
+        
+        const weekday = ["NE","PO","ÚT","ST","ČT","PÁ","SO"];
 
         // header
         let weekdayCounter = 1;
-        createCells(7).forEach(day => {
+        createCells(weekday.length).forEach(day => {
             day.innerText = weekday[weekdayCounter%7];
             weekdayCounter++;
             daysContainer.appendChild(day);
@@ -26,10 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const cols = 7;
         const gridCellCount = rows*cols;
         const firstEmptyCellCount = (firstDayOfMonth.getDay() + 6) % 7;
-
-        console.log(firstDayOfMonth.getDay());
-        console.log(firstEmptyCellCount);
-        console.log(firstDayOfMonth)
 
         createCells(firstEmptyCellCount).forEach(day => {
             day.classList.add('empty');
@@ -51,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         calendar.appendChild(daysContainer);
     }
-    createCalendarGrid();
+    
 
     function createCells(count) {
         const cells = [];
@@ -61,5 +76,18 @@ document.addEventListener('DOMContentLoaded', () => {
             cells.push(day);
         }
         return cells;
+    }
+
+    showCalendar();
+
+    const buttons = document.querySelectorAll('button');
+    buttons[0].addEventListener('click', () => onCalendarButtonClick(true));
+    buttons[1].addEventListener('click', () => onCalendarButtonClick(false));
+
+    function onCalendarButtonClick(isLeft) {
+        if(isLeft) { currentMonth--; }
+        else { currentMonth++; }
+        calendar.removeChild(daysContainer);
+        showCalendar();
     }
 });
