a5fa41a62c5d18f163c8103beaf3c6f6fd704bc5
GedTest <viktorzwinger@seznam.cz>
2024-01-09 07:18:09 +0100

    show, add event

--- script.js
+++ script.js
@@ -25,7 +25,9 @@ document.addEventListener('DOMContentLoaded', () => {
         month.innerText = `${months[firstDayOfMonth.getMonth()]} ${firstDayOfMonth.getFullYear()}`;
 
         createCalendarGrid();
+        colorCells();
     }
+
     function createCalendarGrid() {
         daysContainer.innerHTML = '';
         
@@ -33,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // header
         let weekdayCounter = 1;
-        createCells(weekday.length).forEach(day => {
+        createCells(weekday.length, 'header').forEach(day => {
             day.innerText = weekday[weekdayCounter%7];
             weekdayCounter++;
             daysContainer.appendChild(day);
@@ -42,42 +44,39 @@ document.addEventListener('DOMContentLoaded', () => {
                 
         
         const rows = 6;
-        const cols = 7;
+        const cols = weekday.length;
         const gridCellCount = rows*cols;
         const firstEmptyCellCount = (firstDayOfMonth.getDay() + 6) % 7;
 
-        createCells(firstEmptyCellCount).forEach(day => {
-            day.classList.add('empty');
+        createCells(firstEmptyCellCount, 'empty').forEach(day => {
             daysContainer.appendChild(day);
         });
 
         let counter = 1;
-        createCells(lastDayOfMonth.getDate()).forEach(day => {
+        createCells(lastDayOfMonth.getDate(), 'day').forEach(day => {
             day.innerText = counter++;
             daysContainer.appendChild(day);
         });
 
 
         const c = gridCellCount - lastDayOfMonth.getDate() - firstEmptyCellCount;
-        createCells(c).forEach(day => {
-            day.classList.add('empty');
+        createCells(c, 'empty').forEach(day => {
             daysContainer.appendChild(day);
         });
-
-        calendar.appendChild(daysContainer);
     }
     
-
-    function createCells(count) {
+    function createCells(count, cssClass) {
         const cells = [];
         for(let i = 0; i < count; i++) {
             const day = document.createElement('div');
-            day.classList.add('day');
+            day.classList.add(cssClass);
+            if(cssClass === 'day') {
+                day.addEventListener('click', onCellClick);
+            }
             cells.push(day);
         }
         return cells;
     }
-
     showCalendar();
 
     const buttons = document.querySelectorAll('button');
@@ -87,7 +86,79 @@ document.addEventListener('DOMContentLoaded', () => {
     function onCalendarButtonClick(isLeft) {
         if(isLeft) { currentMonth--; }
         else { currentMonth++; }
-        calendar.removeChild(daysContainer);
         showCalendar();
     }
+
+
+
+
+
+
+
+
+    let dayCell = null;
+    function onCellClick() {
+        
+        const day = this.innerText;
+        const year = firstDayOfMonth.getFullYear();
+        const month = firstDayOfMonth.getMonth();
+        const date = `${year}-${month}-${day}`;
+        
+        const event = localStorage.getItem(date);
+        
+        if (event) {
+            showEvent();            
+        }
+        else {
+            showForm();
+            dayCell = this;
+        }
+        
+    }
+
+    function showForm() {
+        const form = document.querySelector('.form');
+        form.style.display = (form.style.display === 'block' && dayCell === this) ? '' : 'block';
+
+
+        document.querySelector('#save').addEventListener('click', _ => {
+            const eventNameInput = document.querySelector('#eventName');
+            const eventName = eventNameInput.value;
+            localStorage.setItem(date, eventName);
+
+            dayCell.classList.add('event');
+        });
+    }
+
+    function showEvent() {
+        const eventDiv = document.querySelector('.event');
+        const eventTitle = document.querySelector('#title');
+        const eventDate = document.querySelector('#date');
+
+        eventDiv.classList.remove('active');
+        eventTitle.innerText = '';
+        eventDate.innerText = '';
+
+        eventDiv.classList.add('active');
+        eventTitle.innerText = event;
+        eventDate.innerText = date;
+    }
+    
+
+
+    function colorCells() {
+        for (let i = 0; i < lastDayOfMonth.getDate(); i++) {
+            const year = firstDayOfMonth.getFullYear();
+            const m = firstDayOfMonth.getMonth();
+            const date = `${year}-${m}-${i + 1}`;
+
+            
+            const event = localStorage.getItem(date);
+            if (event) {
+                const dayElement = daysContainer.children[i + 7 + ((firstDayOfMonth.getDay() + 6) % 7)];
+                dayElement.classList.add('event');
+            }
+        }
+    }
+    
 });
